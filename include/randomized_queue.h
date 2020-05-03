5f077271bb3989d9b62f36555c59d52884a8d7eb
Mee-Tree <89823187178@yandex.ru>
2020-05-03 19:14:26 +0300

    separate header and implementation

--- include/randomized_queue.h
+++ include/randomized_queue.h
@@ -9,79 +9,15 @@
 template <class T>
 class randomized_queue {
     template <bool is_const>
-    class random_iterator {
-        friend class randomized_queue;
-
-        using begin_type = std::conditional_t<is_const,
-                typename std::vector<T>::const_iterator,
-                typename std::vector<T>::iterator>;
-
-        random_iterator(begin_type begin, std::size_t size, bool is_end = false)
-                : m_begin(begin), m_current(is_end ? size : 0) {
-            m_permutation.resize(size);
-            generate();
-        }
-
-        void generate() {
-            std::iota(m_permutation.begin(), m_permutation.end(), 0);
-            std::shuffle(m_permutation.begin(), m_permutation.end(), get_random_generator());
-            m_permutation.push_back(m_permutation.size());
-        }
-
-    public:
-        using value_type = std::conditional_t<is_const, std::add_const_t<T>, T>;
-        using difference_type = std::ptrdiff_t;
-        using iterator_category = std::forward_iterator_tag;
-        using pointer = value_type *;
-        using reference = value_type &;
-
-        template <bool v>
-        random_iterator(const random_iterator<v> & other)
-                : m_begin(other.m_begin)
-                , m_current(other.m_current)
-                , m_permutation(other.m_permutation) {}
-
-        reference operator * () const {
-            return *(m_begin + m_permutation[m_current]);
-        }
-
-        pointer operator -> () const {
-            return (m_begin + m_permutation[m_current]).operator -> ();
-        }
-
-        random_iterator & operator ++ () {
-            ++m_current;
-            return *this;
-        }
-
-        random_iterator operator ++ (int) {
-            auto tmp(*this);
-            operator ++ ();
-            return tmp;
-        }
-
-        bool operator == (const random_iterator & other) const {
-            return (m_begin + m_permutation[m_current]) == (other.m_begin + other.m_permutation[other.m_current]);
-        }
-        bool operator != (const random_iterator & other) const {
-            return !(operator == (other));
-        }
-
-        ~random_iterator() = default;
-
-    private:
-        begin_type m_begin;
-        std::size_t m_current;
-        std::vector<std::size_t> m_permutation;
-    };
+    class random_iterator;
 
     using iterator = random_iterator<false>;
     using const_iterator = random_iterator<true>;
 
 public:
     randomized_queue() = default;
-    randomized_queue(randomized_queue && other)
-            : m_data(other.m_data) {}
+    randomized_queue(randomized_queue &&);
+    ~randomized_queue() = default;
 
     auto begin()         { return iterator(m_data.begin(), size()); };
     auto end()           { return iterator(m_data.begin(), size(), true); };
@@ -90,48 +26,17 @@ public:
     auto cbegin()  const { return begin(); };
     auto cend()    const { return end(); };
 
-    bool empty() const {
-        return m_data.empty();
-    }
+    bool empty() const;
+    std::size_t size() const;
 
-    std::size_t size() const {
-        return m_data.size();
-    }
-
-    void enqueue(const T & item) {
-        m_data.push_back(item);
-        distribution = get_distribution();
-    }
-
-    void enqueue(T && item) {
-        m_data.emplace_back(std::forward<T>(item));
-        distribution = get_distribution();
-    }
-
-    T const & sample() const {
-        std::size_t ind = const_cast<randomized_queue *>(this)->get_random_index();
-        return m_data[ind];
-    }
-
-    T dequeue() {
-        std::size_t ind = get_random_index();
-        std::swap(m_data[ind], m_data.back());
-        auto tmp = std::move(m_data.back());
-        m_data.pop_back();
-        distribution = get_distribution();
-        return tmp;
-    };
-
-    ~randomized_queue() = default;
+    void enqueue(const T & item);
+    void enqueue(T && item);
+    T const & sample() const;
+    T dequeue();
 
 private:
-    std::uniform_int_distribution<std::size_t> get_distribution() {
-        return std::uniform_int_distribution<std::size_t>(0, size() - 1);
-    }
-
-    std::size_t get_random_index() {
-        return distribution(generator);
-    }
+    std::uniform_int_distribution<std::size_t> get_distribution();
+    std::size_t get_random_index();
 
     static std::mt19937_64 get_random_generator() {
         std::random_device random_device;
@@ -140,6 +45,7 @@ private:
 
     std::mt19937_64 generator = get_random_generator();
     std::uniform_int_distribution<std::size_t> distribution;
-
     std::vector<T> m_data;
 };
+
+#include "randomized_queue.tpp"
